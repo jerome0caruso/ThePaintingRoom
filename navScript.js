8e31b796793523dc5d75aeba0fe9f52772f41b63
Jerome <jerome0caruso@gmail.com>
2021-11-26 08:50:05 -0600

    fixed some mobile issues

--- navScript.js
+++ navScript.js
@@ -1,17 +1,16 @@
 const navBtn = document.querySelector('.hambar');
 const mobileLinks = document.querySelector('.mobileLinks');
-const container = document.querySelector('.section-1');
+const containerSec = document.querySelector('.section-1');
 const container2 = document.querySelector('.container');
 
 
 if(Number(screen.width) < 1900) {
     console.log("small resolution")
-    container.classList.add('smallRes');
+    containerSec.classList.add('smallRes');
     container2.classList.add('smallResM');
 }
 
 function getNav() {
-    console.log("sd")
     mobileLinks.classList.toggle('hidden');
 }
 
